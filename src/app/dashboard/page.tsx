1c22debd85aa07bb3612c20a0c158182a473a796
MaceiraDev <leo.maceira.santos@hotmail.com>
2024-06-24 20:03:18 -0300

    Iniciando mudanças dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 import { LayoutDashboard } from "@/components/LayoutDashboard";
 import { cookies } from 'next/headers'
+import styles from './styles.module.css'
+import { Table } from "react-bootstrap";
 
 export default function Dashboard() {
 
@@ -9,9 +11,67 @@ export default function Dashboard() {
   if (!token) {
     window.location.href = '/';
   }
+
   return (
-    <LayoutDashboard>
-      <h1>Dashboard Saudeeeeee</h1>
-    </LayoutDashboard>
+    <>
+      <LayoutDashboard>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4 mt-3">
+              <div className={styles.card}>
+                <h4>Total de Vacinas <i className="bi bi-clipboard2-pulse"></i></h4>
+                <span>2</span>
+              </div>
+            </div>
+            <div className="col-md-4 mt-3">
+              <div className={styles.card}>
+                <h4>Agendamentos Aceitos <i className="bi bi-clipboard2-plus"></i></h4>
+                <span>2</span>
+              </div>
+            </div>
+            <div className="col-md-4 mt-3">
+              <div className={styles.card}>
+                <h4>Agendamentos Concluídos <i className="bi bi-clipboard2-check"></i></h4>
+                <span>2</span>
+              </div>
+            </div>
+          </div>
+          <div className="row mt-5">
+            <div className="col-md-8">
+              <Table striped bordered hover variant="dark" responsive>
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Username</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td colSpan={2}>Larry the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </div>
+            <div className="col-md-4"></div>
+          </div>
+        </div>
+      </LayoutDashboard>
+    </>
   )
 }
